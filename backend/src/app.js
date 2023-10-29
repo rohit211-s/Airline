2583e7777f066613fc4809f426d5c958a5b82534
Sharan Reddy <sharansaikonda99@gmail.com>
2023-10-29 18:45:28 -0400

    project setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,15 +4,6 @@ const cors = require("cors");
 
 // Global Setup
 require("dotenv").config();
-require("npm-atom").db;
-require("./routers/automaticserver");
-
-// Custom Packages
-const middleware = require("npm-atom").middleware;
-const defaultAuthRouter = require("npm-atom").auth;
-const defaultEmailRouter = require("npm-atom").email;
-const itemRouter = require("./routers/items");
-const reminderRouter = require("./routers/reminder");
 
 // Setup
 const app = express();
@@ -20,10 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(middleware.InitAPILoggerMiddleware);
-app.use("/api/auth", defaultAuthRouter);
-app.use("/api/email", defaultEmailRouter);
-app.use("/api/items", itemRouter);
-app.use("/api/reminders", reminderRouter);
+app.get("/api", async (req, res) => {
+  res.status(200).send({ msg: "Hello Sharan Reddy" });
+});
 
 module.exports = app;
